50f61f2c9878084eec8ce8b656fc360b2202048f
Mustafa Yıldız <hasansabbah284@gmail.com>
2021-12-28 02:36:05 +0300

    partial ejs

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
-
+const path = require('path');
 const app = express();
+const ejs = require('ejs');
 
 const port = 3000;
-
+app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 app.get('/',(req,res)=> {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" };
-
-    res.send(blog);
+    res.render('index');
 })
+app.get("/about", (req, res) => {
+    res.render('about');
+  });
+  
+  app.get("/add_post", (req, res) => {
+    res.render('add_post');
+  });
 
 app.listen(port,()=> {
     console.log("Port dinleniyor");
