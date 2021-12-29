f528db03bab731e87731ed79a5f77965f5a13c5f
Mustafa Yıldız <hasansabbah284@gmail.com>
2021-12-29 06:14:46 +0300

    post and get from db

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const express = require('express')
-const path = require('path');
+const Photo = require('./models/Photo');
+
 const app = express();
 const ejs = require('ejs');
-
+const mongoose = require('mongoose');
 const port = 3000;
+
+
 app.use(express.static('public'));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get('/',(req,res)=> {
-    res.render('index');
-})
+
+mongoose.connect('mongodb://localhost/pcat-deneme-db',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+});
+
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
+});
 app.get("/about", (req, res) => {
     res.render('about');
   });
   
   app.get("/add_post", (req, res) => {
     res.render('add_post');
+  });
+
+  app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)  
+    res.redirect('/')
   });
 
 app.listen(port,()=> {
