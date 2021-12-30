d23fa4bbb69c0561ce445ee3b25e82ab0bffad97
Mustafa Yıldız <hasansabbah284@gmail.com>
2021-12-30 09:00:43 +0300

    mvc

--- app.js
+++ app.js
@@ -1,49 +1,39 @@
-const express = require('express')
-const Photo = require('./models/Photo');
-
+const express = require("express");
+const Photo = require("./models/Photo");
+const fileUpload = require("express-fileupload"); // modülü kullanıma alıyoruz.
 const app = express();
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 const port = 3000;
+const fs = require("fs");
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
 
 
-app.use(express.static('public'));
+app.use(fileUpload());
+app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+mongoose.connect("mongodb://localhost/pcat-deneme-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 
-mongoose.connect('mongodb://localhost/pcat-deneme-db',{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
 });
 
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index', {
-    photos
-  });
+
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+app.listen(port, () => {
+  console.log("Port dinleniyor");
 });
-app.get("/about", (req, res) => {
-    res.render('about');
-  });
-  
-  app.get("/add_post", (req, res) => {
-    res.render('add_post');
-  });
-
-  app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)  
-    res.redirect('/')
-  });
-
-  app.get("/photos/:id", (req, res) => {
-    console.log(req.params.id);
-  });
-
-
-
-app.listen(port,()=> {
-    console.log("Port dinleniyor");
-});
-
